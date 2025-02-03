1d562125c9e27e0e5ef970cfd9eb177e421a8804
anouar garbaa <g.anouar@yahoo.com>
2025-02-03 15:16:36 +0100

    Implement the Search Page

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">GiftLink</a>
+            <Link className="navbar-brand" to="/">GiftLink</Link>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
 
@@ -11,7 +12,10 @@ export default function Navbar() {
                     <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}
+                        <Link className="nav-link" to="/app">Gifts</Link> {/* Updated Link */}
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/app/search">Search</Link>
                     </li>
                 </ul>
             </div>
